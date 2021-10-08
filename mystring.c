9b4aaa89313a03f927a4118773be61243ac230bb
KenLi8504 <kli22@stuy.edu>
2021-10-08 05:18:14 -0400

    better version of mystrpcmp

--- mystring.c
+++ mystring.c
@@ -16,15 +16,14 @@ int mystrlen(char *s){
 }
 
 int mystrcmp(char *s1, char *s2) {
-    char *cs1 = s1;
-    char *cs2 = s2;
-    while ((*cs1 == *cs2) && *cs1 != '\0') {
-        cs1++; cs2++;
+    while ((*s1 == *s2) && *s1 != '\0') {
+        *s1++;
+        *s2++;
     }
-    if (*cs1 > *cs2){
+    if (*s1 > *s2){
       return 1;
     }
-    if (*cs1 < *cs2){
+    else if (*s1 < *s2){
       return -1;
     }
     else{
@@ -33,17 +32,17 @@ int mystrcmp(char *s1, char *s2) {
 }
 
 int main(){
-  // char test[20] = "Hello";
-  // char s1 [5] = "Bnt";
-  // char s2 [5] = "Ant";
-  // char s3 [5] = "Ant";
-  // char s4 [5] = "Ants";
-  // printf("the result of %s vs %s is %d \n",s1,s2,mystrcmp(s1,s2));
-  // printf("the correct result is %d \n",strcmp(s1,s2));
-  // printf("the result of %s vs %s is %d \n",s2,s1,mystrcmp(s2,s1));
-  // printf("the correct result is %d \n",strcmp(s2,s1));
-  // printf("the result of %s vs %s is %d \n",s2,s3,mystrcmp(s2,s3));
-  // printf("the correct result is %d \n",strcmp(s2,s3));
-  // printf("the result of %s vs %s is %d \n",s2,s4,mystrcmp(s2,s4));
-  // printf("the correct result is %d \n",strcmp(s2,s4));
+  char test[20] = "Hello";
+  char s1 [5] = "Bnt";
+  char s2 [5] = "Ant";
+  char s3 [5] = "Ant";
+  char s4 [5] = "Ants";
+  printf("the result of %s vs %s is %d \n",s1,s2,mystrcmp(s1,s2));
+  printf("the correct result is %d \n",strcmp(s1,s2));
+  printf("the result of %s vs %s is %d \n",s2,s1,mystrcmp(s2,s1));
+  printf("the correct result is %d \n",strcmp(s2,s1));
+  printf("the result of %s vs %s is %d \n",s2,s3,mystrcmp(s2,s3));
+  printf("the correct result is %d \n",strcmp(s2,s3));
+  printf("the result of %s vs %s is %d \n",s2,s4,mystrcmp(s2,s4));
+  printf("the correct result is %d \n",strcmp(s2,s4));
 }
